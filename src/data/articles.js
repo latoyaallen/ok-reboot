f3ddbba07b888669424cc5ac87df8d64ecd8dc8a
latoyaallen <me@tailorlabs.io>
2020-06-09 15:37:58 -0400

    Clean up data

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,24 +1,10 @@
-import Brooklinen__WeightedBlanket
- from '../images/Brooklinen__WeightedBlanket.png';
-import Lunya__Nightstand
- from '../images/Lunya__Nightstand.png';
-import Princeton__Gardening
- from '../images/Princeton__Gardening.png';
 import SweetPotatoSoul__balls
  from '../images/SweetPotatoSoul__balls.png';
-import MensHealth__Mobility
- from '../images/MensHealth__Mobility.png';
-import EastRiverPilates__Online
- from '../images/EastRiverPilates__Online.png';
 import Golde__MatchaLoafCake
  from '../images/Golde__MatchaLoafCake.png';
-import Tula__Pruning
- from '../images/Tula__Pruning.png';
-import Tula__PlantLibrary
- from '../images/Tula__PlantLibrary.png';
-
 const articles = [
   {
+    // Trinity Mouzon Wofford
     "id":"1",
     "title":"Make this: Easy Pure Matcha Loaf Cake",
     "about":"This cozy loaf recipe is our new go-to for weekends at home.",
@@ -28,42 +14,6 @@ const articles = [
     "img": Golde__MatchaLoafCake,
   },
   {
-    "id":"2",
-    "title":"East River Pilates Online",
-    "about":"Try their free Instant Feel Good videos.",
-    "url":"https://eastriverpilates.com/online",
-    "source": "East River Pilates",
-    "category":"Fitness",
-    "img": EastRiverPilates__Online,
-  },
-  {
-    "id":"3",
-    "title":"Sowing Seeds of Happines",
-    "about":"Emotional well-being while home gardening similar to other popular activities, study finds",
-    "url":"https://environment.princeton.edu/news/emotional-well-being-while-home-gardening-similar-to-other-popular-activities-study-finds/",
-    "source": "Princeton Environmental Institute",
-    "category":"Plants",
-    "img": Princeton__Gardening,
-  },
-  {
-    "id":"4",
-    "title":"Plant Sessions: Pruning",
-    "about":"How do you prune plants? When? Why? With what?",
-    "url":"https://vimeo.com/403086744",
-    "source": "Tula House",
-    "category":"Plants",
-    "img": Tula__Pruning,
-  },
-  {
-    "id":"5",
-    "title":"Nightstand Essentials",
-    "about":"Add these to your bedtime routine",
-    "url":"https://www.lunya.co/blogs/our-bedroom/lunya-nightstand-essentials",
-    "source": "Lahgo",
-    "category":"Sleep",
-    "img": Lunya__Nightstand,
-  },
-  {
     "id":"6",
     "title":"Energizing Coco Date Balls",
     "url":"https://sweetpotatosoul.com/2018/05/energizing-coco-date-balls.html",
@@ -71,33 +21,6 @@ const articles = [
     "source": "Sweet Potato Soul",
     "category":"Food",
     "img": SweetPotatoSoul__balls,
-  },
-  {
-    "id":"7",
-    "title":"This Simple Mobility Flow Will Help You Stay Loose at Home",
-    "url":"https://www.menshealth.com/fitness/a31932672/mobility-flow-at-home/",
-    "about":"Men's Health Advisory Board member Dan Giordano leads us through his go-to routine",
-    "source": "Mens Health",
-    "category":"Fitness",
-    "img": MensHealth__Mobility,
-  },
-  {
-    "id":"8",
-    "title":"Considering Getting a Weighted Comforter? Here's Why You Should",
-    "about":"A good nights rest is just around the corner",
-    "url":"https://www.brooklinen.com/blogs/brookliving/considering-getting-a-weighted-blanket-heres-why-you-should",
-    "source": "Brooklinen",
-    "category":"Sleep",
-    "img": Brooklinen__WeightedBlanket,
-  },
-  {
-    "id":"9",
-    "title":"Tula's Plant Library",
-    "about":"Troubleshoot plant questions and feed your growing curiosity.",
-    "url":"https://tula.house/blogs/tulas-plant-library",
-    "source": "Tula House",
-    "category":"Plants",
-    "img": Tula__PlantLibrary,
   },
 ];
 
