df47f8eb14beff43b59a66c60a95508817265cf0
latoyaallen <me@tailorlabs.io>
2020-06-11 19:17:44 -0400

    Add routes for Content Menu

--- src/components/App.js
+++ src/components/App.js
@@ -58,6 +58,44 @@ const styles = StyleSheet.create({
 });
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {view: 'home'};
+    this.handleHome = this.handleHome.bind(this);
+  }
+
+  handleHome() {
+    window.location.assign(`/`)
+    this.setState({view: 'home'});
+  }
+
+  componentDidMount() {
+    const pathname = window.location.pathname.substring(1,24);
+
+    if(pathname === 'body') {
+      this.setState({view: 'body'});
+    }
+
+    if(pathname === 'belly') {
+      this.setState({view: 'belly'});
+    }
+
+    if(pathname === 'mind') {
+      this.setState({view: 'mind'});
+    }
+
+    if(pathname === 'space') {
+      this.setState({view: 'space'});
+    }
+
+    if(pathname === 'about') {
+      this.setState({view: 'about'});
+    }
+
+    if(pathname === 'home') {
+      this.setState({view: 'home'});
+    }
+  }
 
   render() {
     let view;
