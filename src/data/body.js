9dab66a6add2fbbd3ffe45659c69aa9a05d1d6be
latoyaallen <me@tailorlabs.io>
2020-06-11 09:47:52 -0400

    Add all needed data files

---
+++ src/data/body.js
@@ -0,0 +1,16 @@
+ from '../images/Golde__MatchaLoafCake.png';
+import IsaWelly__GentlePilates
+ from '../images/IsaWelly__GentlePilates.png';
+const body = [
+  {
+    "id":"1",
+    "title":"Gentle Morning Pilates",
+    "about":"",
+    "url":"https://www.youtube.com/watch?v=Jdu0GBht-7g&feature=youtu.be",
+    "source": "IsaWelly Pilates",
+    "category":"Body",
+    "img": IsaWelly__GentlePilates,
+  },
+];
+
+export default body;
