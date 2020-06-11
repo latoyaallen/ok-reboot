f6604fee49857ab49fb5278fa942456e723ad48d
latoyaallen <me@tailorlabs.io>
2020-06-11 09:12:11 -0400

    Update categories

--- src/data/articles.js
+++ src/data/articles.js
@@ -2,16 +2,20 @@ import SweetPotatoSoul__balls
  from '../images/SweetPotatoSoul__balls.png';
 import Golde__MatchaLoafCake
  from '../images/Golde__MatchaLoafCake.png';
+import IsaWelly__GentlePilates
+ from '../images/IsaWelly__GentlePilates.png';
+import Jungalow__philodendron
+ from '../images/Jungalow__philodendron.png';
 const articles = [
   {
     // Trinity Mouzon Wofford
     "id":"1",
-    "title":"Easy Pure Matcha Loaf Cake",
-    "about":"This cozy loaf recipe is our new go-to for weekends at home.",
-    "url":"https://golde.co/blogs/the-golden-hour/make-this-pure-matcha-loaf-cake",
-    "source": "Golde",
-    "category":"Food",
-    "img": Golde__MatchaLoafCake,
+    "title":"Gentle Morning Pilates",
+    "about":"",
+    "url":"https://www.youtube.com/watch?v=Jdu0GBht-7g&feature=youtu.be",
+    "source": "IsaWelly Pilates",
+    "category":"Body",
+    "img": IsaWelly__GentlePilates,
   },
   {
     "id":"2",
@@ -19,8 +23,8 @@ const articles = [
     "url":"https://blog.jungalow.com/2020/05/plant-o-pedia-philodendron-brasil.html",
     "about":"",
     "source": "The Jungalow",
-    "category":"Plants",
-    "img": SweetPotatoSoul__balls,
+    "category":"Space",
+    "img": Jungalow__philodendron,
   },
   {
     // Trinity Mouzon Wofford
@@ -29,7 +33,7 @@ const articles = [
     "about":"This cozy loaf recipe is our new go-to for weekends at home.",
     "url":"https://golde.co/blogs/the-golden-hour/make-this-pure-matcha-loaf-cake",
     "source": "Golde",
-    "category":"Food",
+    "category":"Belly",
     "img": Golde__MatchaLoafCake,
   },
   {
@@ -38,9 +42,10 @@ const articles = [
     "url":"https://sweetpotatosoul.com/2018/05/energizing-coco-date-balls.html",
     "about":"Jenne Claiborne shares a recipe we love",
     "source": "Sweet Potato Soul",
-    "category":"Food",
+    "category":"Belly",
     "img": SweetPotatoSoul__balls,
   },
+
 ];
 
 export default articles;
