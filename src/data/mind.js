dd60050b028bffec347004c2b32e4b7c7af7e559
latoyaallen <me@tailorlabs.io>
2020-06-11 15:29:12 -0400

    Update mind data

---
+++ src/data/mind.js
@@ -0,0 +1,33 @@
+import Jungalow__philodendron
+ from '../images/Jungalow__philodendron.png';
+const mind = [
+  {
+    "id":"2",
+    "title":"LobaLand x The Nap Ministry present: REST then RISE guided meditation for Revolution",
+    "url":"https://anchor.fm/lobaland/episodes/LIBERATION-June-guided-meditation-eepvgr",
+    "about":"",
+    "source": "Lotus LaLoba",
+    "category":"mind",
+    "img": Jungalow__philodendron,
+  },
+  {
+    "id":"2",
+    "title":"5 Sense Meditation || Travelasana with Malaika",
+    "url":"https://www.youtube.com/watch?v=ieulO8pVrkQ&feature=youtu.be",
+    "about":"",
+    "source": "Malaika Brooks-Smith-Lowe",
+    "category":"mind",
+    "img": Jungalow__philodendron,
+  },
+  {
+    "id":"2",
+    "title":"I AM Morning Affirmations for Women",
+    "url":"https://www.youtube.com/watch?v=j71q6zlbO3I&feature=youtu.be",
+    "about":"",
+    "source": "Alanna Foxx",
+    "category":"mind",
+    "img": Jungalow__philodendron,
+  },
+];
+
+export default mind;
