6be19819f7abdb1c1d9c3adb8566f8d9273a2f14
latoyaallen <me@tailorlabs.io>
2020-06-21 12:34:11 -0400

    Update PositivityAd.js

--- src/components/PositivityAd.js
+++ src/components/PositivityAd.js
@@ -10,7 +10,7 @@ import {
 
 const styles = StyleSheet.create({
   PositivityAd__container: {
-    paddingBottom: '1.9em',
+    paddingBottom: '5.9em',
     [MOBILE_PORTRAIT]: {
     },
     [MOBILE_LANDSCAPE]: {
@@ -43,15 +43,13 @@ const styles = StyleSheet.create({
   },
 });
 
-const text =
-      "Spread good vibes today. Take some of my positivity.  Today is going to be a sucessful day.  And you're going to be satisfied at the end of the day. We're going to crush it. We need more people like you on the planet."
-
 const PositivityAd = () => (
   <div className={css(styles.PositivityAd__container)}>
     <div className={css(styles.PositivityAd__text)}>
       <p>Spread good vibes today.</p>
-      <p>Take some of my positivity.  Today is going to be a sucessful day.  And you're going to be satisfied at the end of the day.</p>
-
+      <p>Take some of my positivity.</p>
+      <p>Today is going to be a sucessful day,</p>
+      <p>and you're going to be satisfied at the end of the day.</p>
     </div>
   </div>
 )
